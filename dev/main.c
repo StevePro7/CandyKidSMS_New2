7406a621f489ff68ef6ba53f131e46182ef66751
StevePro7 <steveprozune@hotmail.com>
2020-03-21 12:33:38 +0000

    no message

--- dev/main.c
+++ dev/main.c
@@ -10,9 +10,13 @@ void main (void)
 	devkit_SMS_init();
 	devkit_SMS_displayOff();
 
+
 	devkit_SMS_setSpriteMode( devkit_SPRITEMODE_NORMAL() );
 	devkit_SMS_useFirstHalfTilesforSprites_False();
 	devkit_SMS_VDPturnOnFeature( devkit_VDPFEATURE_HIDEFIRSTCOL() );
+
+
+	devkit_SMS_setSpritePaletteColor( 0, 3, 3, 3 );
 
 	//engine_content_manager_load_tiles_font();
 	////engine_content_manager_load_tiles_game();
