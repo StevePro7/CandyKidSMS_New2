fbebd08c600fe196563cc31778a44771dbb4c454
StevePro7 <steveprozune@hotmail.com>
2020-03-21 12:23:48 +0000

    no message

--- dev/main.c
+++ dev/main.c
@@ -4,9 +4,9 @@ void main (void)
 {
 	// Global variables.
 	static bool global_pause;
-	unsigned char open_screen_type;
+	//unsigned char open_screen_type;
 
-	engine_asm_manager_clear_VRAM();
+	//engine_asm_manager_clear_VRAM();
 	devkit_SMS_init();
 	devkit_SMS_displayOff();
 
@@ -14,21 +14,21 @@ void main (void)
 	devkit_SMS_useFirstHalfTilesforSprites_False();
 	devkit_SMS_VDPturnOnFeature( devkit_VDPFEATURE_HIDEFIRSTCOL() );
 
-	engine_content_manager_load_tiles_font();
-	//engine_content_manager_load_tiles_game();
-	engine_content_manager_load_sprites_game();
+	//engine_content_manager_load_tiles_font();
+	////engine_content_manager_load_tiles_game();
+	//engine_content_manager_load_sprites_game();
 
-	engine_state_manager_init();
-	engine_state_manager_read();
+	//engine_state_manager_init();
+	//engine_state_manager_read();
 
 	//open_screen_type = screen_type_intro;
 	//open_screen_type = screen_type_func;
 	//open_screen_type = screen_type_test;
 	//open_screen_type = screen_type_splash;
-	open_screen_type = screen_type_begin;
+	//open_screen_type = screen_type_begin;
 	//open_screen_type = screen_type_title;
 	//open_screen_type = screen_type_diff;
-	open_screen_type = screen_type_init;
+	//open_screen_type = screen_type_init;
 	//open_screen_type = screen_type_load;
 	//open_screen_type = screen_type_ready;
 	//open_screen_type = screen_type_demo;
@@ -43,7 +43,7 @@ void main (void)
 	//open_screen_type = screen_type_pass;
 	//open_screen_type = screen_type_beat;
 
-	engine_screen_manager_init( open_screen_type );
+	//engine_screen_manager_init( open_screen_type );
 	devkit_SMS_displayOn();
 	for( ;; )
 	{
@@ -67,8 +67,8 @@ void main (void)
 		}
 
 		devkit_SMS_initSprites();
-		engine_input_manager_update();
-		engine_screen_manager_update();
+		//engine_input_manager_update();
+		//engine_screen_manager_update();
 
 		devkit_SMS_finalizeSprites();
 		devkit_SMS_waitForVBlank();
