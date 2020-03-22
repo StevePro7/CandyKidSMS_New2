8f156556ca157d7cff654d0cba3ba6dd519c47d3
StevePro7 <steveprozune@hotmail.com>
2020-03-22 10:08:43 +0000

    no message

--- dev/scripts/gfx.bat
+++ dev/scripts/gfx.bat
@@ -8,11 +8,9 @@ cd gfx
 :: Tiles
 bmp2tile.exe raw\font_tiles.bmp -savetiles "font_tiles (tiles).psgcompr" -noremovedupes -planar -tileoffset 0  -savetilemap "font_tiles (tilemap).bin" -savepalette "font_tiles (palette).bin" -fullpalette -exit
 bmp2tile.exe raw\game_tiles.png -savetiles "game_tiles (tiles).psgcompr" -noremovedupes -planar -tileoffset 64 -savetilemap "game_tiles (tilemap).bin" -savepalette "game_tiles (palette).bin" -fullpalette -exit
-::bmp2tile.exe raw\main_tiles.png -savetiles "main_tiles (tiles).psgcompr" -noremovedupes -planar -tileoffset 64 -savetilemap "main_tiles (tilemap).bin" -savepalette "main_tiles (palette).bin" -fullpalette -exit
 
 
 :: Sprites
-:: https://github.com/maxim-zhao/bmp2tile
 bmp2tile.exe raw\sprites.png -savetiles "sprites (tiles).psgcompr" -noremovedupes -nomirror -planar -tileoffset 0 -savepalette "sprites (palette).bin" -fullpalette  -spritepalette -exit
 
 cd ..\dev
