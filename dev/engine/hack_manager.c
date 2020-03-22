92bfd655aeeba99daa9f7da9fecf41e74d091245
StevePro7 <steveprozune@hotmail.com>
2020-03-22 16:27:09 +0000

    no message

--- dev/engine/hack_manager.c
+++ dev/engine/hack_manager.c
@@ -86,7 +86,7 @@ void engine_hack_manager_invert()
 	//st->state_object_enemy_move[ actor_type_suz ] = 0;
 
 
-	st->state_object_mydebugger = 1;		// TODO don't forget to remove this!!
+	st->state_object_mydebugger = 1;		// TODO don't forget to remove this!!		MUST be zero for final build as pass music will NOT play!!
 	st->state_object_full_boost = 0;
 	st->state_object_delay_test = 0;
 	st->state_object_load_asset = 0;
@@ -130,8 +130,8 @@ void engine_hack_manager_invert()
 	// TODO revert this code to play music + sound FX.
 
 	// TODO delete this hard coded
-	st->state_object_world_data = 2 - 1;
-	st->state_object_round_data = 5 - 1;
+	st->state_object_world_data = 1 - 1;
+	st->state_object_round_data = 1 - 1;
 
 	//st->state_object_world_data = 10 - 1;
 	//st->state_object_round_data = 9 - 1;
