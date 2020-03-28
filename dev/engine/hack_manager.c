a7ba99a006593b3138f838b0de6d1e37391c8d21
StevePro7 <steveprozune@hotmail.com>
2020-03-28 17:23:43 +0000

    no message

--- dev/engine/hack_manager.c
+++ dev/engine/hack_manager.c
@@ -47,27 +47,6 @@ void engine_hack_manager_load()
 {
 	struct_hack_object *ho = &global_hack_object;
 
-	/*
-	ho->hack_object_delay_test = PEEK( HACKER_START - 2 );			// 0x004E		// Used to speed through any game delay.
-	ho->hack_object_mydebugger = PEEK( HACKER_START - 1 );			// 0x004F		// Used to show debugging info for game.
-	ho->hack_object_invincibie = PEEK( HACKER_START + 0 );			// 0x0050		// Non-zero value enables invincibility.
-	ho->hack_object_full_boost = PEEK( HACKER_START + 1 );			// 0x0051		// Non-zero value enables maximum boost.
-
-	ho->hack_object_trees_type = PEEK( HACKER_START + 2 );			// 0x0052		// Set value to 1=Show otherwise 2=Kill.
-	//ho->hack_object_exits_type = PEEK( HACKER_START + 3 );			// 0x0053		// Set value to 1=Open otherwise 2=Shut.
-	//ho->hack_object_difficulty = PEEK( HACKER_START + 4 );			// 0x0054		// Set value to 1=Easy otherwise 2=Hard.
-	//ho->hack_object_pace_speed = PEEK( HACKER_START + 5 );			// 0x0055		// Set value to 1=Slow otherwise 2=Fast.
-
-	//ho->hack_object_world_data = PEEK( HACKER_START + 6 );			// 0x0056		// Set start World no currently 1 to 10.
-	//ho->hack_object_round_data = PEEK( HACKER_START + 7 );			// 0x0057		// Set start Round no currently 1 to 10.
-	ho->hack_object_music_data = PEEK( HACKER_START + 8 );			// 0x0058		// Set 0=Music to play otherwise silent.
-	ho->hack_object_sound_data = PEEK( HACKER_START + 9 );			// 0x0059		// Set 0=Sound to play otherwise silent.
-
-	ho->hack_object_enemy_move[ actor_type_pro ] = PEEK( HACKER_START + 16 );
-	ho->hack_object_enemy_move[ actor_type_adi ] = PEEK( HACKER_START + 17 );
-	ho->hack_object_enemy_move[ actor_type_suz ] = PEEK( HACKER_START + 18 );
-	*/
-
 #ifndef _CONSOLE
 
 	ho->hack_object_delay_test = PEEK( HACKER_START - 2 );			// 0x004E		// Used to speed through any game delay.
@@ -180,8 +159,8 @@ void engine_hack_manager_invert()
 	//ho->hack_object_sound_data = 0;
 
 
-	// TODO delete this hard coded
-	//st->state_object_world_data = 1 - 1;
-	//st->state_object_round_data = 1 - 1;
-	// TODO delete this hard coded
+	// TODO delete this hard coded!!
+	//st->state_object_world_data = 7 - 1;
+	//st->state_object_round_data = 4 - 1;
+	// TODO delete this hard coded!!
 }
