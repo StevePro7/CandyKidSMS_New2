d74616a5c4395039b07e658681e5b329b961159d
StevePro7 <steveprozune@hotmail.com>
2020-04-16 13:36:32 +0100

    no message

--- dev/game.h
+++ dev/game.h
@@ -4,11 +4,11 @@
 #define productCode	9999
 #define revision	0
 
-#define verMaj		1
+#define verMaj		2
 #define verMin		0
 #define dateYear	2020
-#define dateMonth	3
-#define dateDay		27
+#define dateMonth	4
+#define dateDay		4
 #define author		"Steven Boland"
 #define name		"Candy Kid"
 #define descr		"Candy Kid game for the SMS Power! 2020 Competition"
